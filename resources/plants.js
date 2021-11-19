b6d2292d55137f54e9d9097ba132a4f77259b69a
zachnkck <90000333+zachnkck@users.noreply.github.com>
2021-11-18 20:32:38 -0600

    testing suite added, hides "start game" and "test game" buttons when beginning, line breaks between "advance" and "reset"

--- resources/plants.js
+++ resources/plants.js
@@ -4,6 +4,9 @@ var activePlant = 0;
 var activeTab;
 var actionPoints = 3;
 
+var winLoss = 0;
+var testingMode = false;
+
 var waterShortage = false;
 var timesWatered = 0;
 
@@ -42,6 +45,7 @@ class Plant
 
 window.onload = createPlants();
 
+
 function randomInRange(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -61,87 +65,91 @@ function advanceDay()
     if(Math.random() < 0.1) // 10% chance of any random event occurring
     {
         actionPoints = 3;
-        switch(randomInRange(1, 8))
-        {
-            case 1:
-                alert("Oh no, there's a water shortage! You can only water one plant today!");
-                waterShortage = true;
-                break;
-            case 2:
-                sunEvent = true;
-                alert("Oh no, it's a cloudy day! All of your plants lose sunlight!");
-                for (const p of plantArr)
-                {
-                    p.sun -= randomInRange(9, 18);
-                    if(p.sun <= 0) // Insta-game-over prevention
-                    {
-                        p.sun = 1;
-                    }
-                }
-                break;
-            case 3:
-                var sick = randomInRange(0, 4);
-                var sickPlant = plantArr[sick];
-                if(sickPlant.name != "[unnamed]")
-                {
-                    alert(`Oh no, ${sickPlant.name} became sick and lost half its health!`);
-                }
-                else
-                {
-                    alert(`Oh no, plant #${sick + 1} became sick and lost half its health!`);
-                }
-                plantArr[sick].water = Math.round(plantArr[sick].water / 2)
-                plantArr[sick].sun = Math.round(plantArr[sick].sun / 2);
-                break;
-            case 4:
-                alert("Oh no, you're feeling sick today! You only get one action point!");
-                actionPoints = 1;
-                break;
-            case 5:
-                waterEvent = true;
-                alert("Good news, it's a rainy day! All of your plants regain water!");
-                for (const p of plantArr)
-                {
-                    p.water += randomInRange(25, 40);
-                    if(p.water > 100) // Overflow prevention
-                    {
-                        p.water = 100;
-                    }
-                }
-                break;
-            case 6:
-                sunEvent = true;
-                alert("Good news, the sun is strong today! All of your plants regain sunlight!");
-                for (const p of plantArr)
-                {
-                    p.sun += randomInRange(25, 40);
-                    if(p.sun > 100) // Insta-game-over prevention
-                    {
-                        p.sun = 100;
-                    }
-                }
-                break;
-            case 7:
-                var lucky = randomInRange(0, 4);
-                var luckyPlant = plantArr[lucky];
-                alert(`Good news, plant #${lucky + 1} has not lost any health today!`);
-                luckyPlant.exempt = true;
-                break;
-            default:
-                alert("Error, if you're reading this, I messed up somehow.");
-                break;
-        }
+		if(!testingMode) {
+			switch(randomInRange(1, 8))
+			{
+				case 1:
+					alert("Oh no, there's a water shortage! You can only water one plant today!");
+					waterShortage = true;
+					break;
+				case 2:
+					sunEvent = true;
+					alert("Oh no, it's a cloudy day! All of your plants lose sunlight!");
+					for (const p of plantArr)
+					{
+						p.sun -= randomInRange(9, 18);
+						if(p.sun <= 0) // Insta-game-over prevention
+						{
+							p.sun = 1;
+						}
+					}
+					break;
+				case 3:
+					var sick = randomInRange(0, 4);
+					var sickPlant = plantArr[sick];
+					if(sickPlant.name != "[unnamed]")
+					{
+						alert(`Oh no, ${sickPlant.name} became sick and lost half its health!`);
+					}
+					else
+					{
+						alert(`Oh no, plant #${sick + 1} became sick and lost half its health!`);
+					}
+					plantArr[sick].water = Math.round(plantArr[sick].water / 2)
+					plantArr[sick].sun = Math.round(plantArr[sick].sun / 2);
+					break;
+				case 4:
+					alert("Oh no, you're feeling sick today! You only get one action point!");
+					actionPoints = 1;
+					break;
+				case 5:
+					waterEvent = true;
+					alert("Good news, it's a rainy day! All of your plants regain water!");
+					for (const p of plantArr)
+					{
+						p.water += randomInRange(25, 40);
+						if(p.water > 100) // Overflow prevention
+						{
+							p.water = 100;
+						}
+					}
+					break;
+				case 6:
+					sunEvent = true;
+					alert("Good news, the sun is strong today! All of your plants regain sunlight!");
+					for (const p of plantArr)
+					{
+						p.sun += randomInRange(25, 40);
+						if(p.sun > 100) // Insta-game-over prevention
+						{
+							p.sun = 100;
+						}
+					}
+					break;
+				case 7:
+					var lucky = randomInRange(0, 4);
+					var luckyPlant = plantArr[lucky];
+					alert(`Good news, plant #${lucky + 1} has not lost any health today!`);
+					luckyPlant.exempt = true;
+					break;
+				default:
+					alert("Error, if you're reading this, I messed up somehow.");
+					break;
+			}
+		}
     }
     else
     {
         actionPoints = 3;
     }
 
-    if(wrongSoilCount >= 12 && !wrongSoilAlerted) // Alert the player near the beginning of the game if their plants are doing badly due to the soil types. The worse, the earlier.
-    {
-        alert("You've noticed that your friend has made questionable soil choices for some of his plants, and that some may be losing water more quickly than others because of it. Perhaps you should try to fix that.");
-        wrongSoilAlerted = true;
-    }
+	if(!testingMode) {
+		if(wrongSoilCount >= 12 && !wrongSoilAlerted) // Alert the player near the beginning of the game if their plants are doing badly due to the soil types. The worse, the earlier.
+		{
+			alert("You've noticed that your friend has made questionable soil choices for some of his plants, and that some may be losing water more quickly than others because of it. Perhaps you should try to fix that.");
+			wrongSoilAlerted = true;
+		}
+	}
 
     curDay++;
     document.getElementById("dayCounter").innerHTML = curDay; // Update the current day counter on the page.
@@ -173,49 +181,67 @@ function advanceDay()
 
         if(p.water <= 25)
         {
-            alert("Be careful! One of your plants is dangerously low on water!");
+			if(!testingMode) {
+				alert("Be careful! One of your plants is dangerously low on water!");
+			}
         }
 
         if(p.sun <= 10)
         {
-            alert("Be careful! One of your plants isn't getting enough sunlight!");
+			if(!testingMode) {
+				alert("Be careful! One of your plants isn't getting enough sunlight!");
+			}
         }
 
         if(p.sun > 100)
         {
-            alert("Be careful! One of your plants is getting too much sunlight!");
+			if(!testingMode) {
+				alert("Be careful! One of your plants is getting too much sunlight!");
+			}
         }
 
         if(p.water <= 0)
         {
-            alert("You neglected one of your plants and it ran out of water. You lose!");
+			if(!testingMode) {
+				alert("You neglected one of your plants and it ran out of water. You lose!");
+			}
+			winLoss = -1;
             window.location.reload();
         }
 
         if(p.sun <= 0)
         {
-            alert("One of your plants got no sunlight and withered away. You lose!");
+			if(!testingMode) {
+				alert("One of your plants got no sunlight and withered away. You lose!");
+			}
+			winLoss = -1;
             window.location.reload();
         }
 
         if(p.sun >= 110)
         {
-            alert("You let one of your plants cook in the sun and it died. You lose!");
+			if(!testingMode) {
+				alert("You let one of your plants cook in the sun and it died. You lose!");
+			}
+			winLoss = -1;
             window.location.reload();
         }
 
         else if(curDay == 40)
-        {
-            if (confirm("Congratulations, you've gotten to day 40, and all your friend's plants are alive! You win! Press OK to play infinitely and take care of the plants, or cancel to quit now."))
-            {
-                alert("Alright, have fun!");
-            }
-            else
-            {
-                curDay++; // Avoiding an infinite loop bug.
-                alert("Thanks for playing!");
-                window.location.reload();
-            }
+        {	
+			winLoss = 1;
+			if(!testingMode) {
+				if (confirm("Congratulations, you've gotten to day 40, and all your friend's plants are alive! You win! Press OK to play infinitely and take care of the plants, or cancel to quit now."))
+				{
+					alert("Alright, have fun!");
+				}
+				else
+				{
+					curDay++; // Avoiding an infinite loop bug.
+					alert("Thanks for playing!");
+					window.location.reload();
+				}
+			}
         }
         p.exempt = false;
     }
@@ -226,14 +252,18 @@ function advanceDay()
 
 function gameHandler()
 {
+	document.getElementById("testing").setAttribute("style", "display:none;");
+	document.getElementById("start1").setAttribute("style", "display:none;");
     document.getElementById("ingame").removeAttribute("hidden"); // Start the game by unhiding the entire game panel.
     document.getElementById("gamepanel").removeAttribute("hidden");
     document.getElementById("dayCounter").innerHTML = curDay;
     document.getElementById("actpt").innerHTML = actionPoints;
-    alert(`Here's the deal: your friend has taken off on a 40-day study abroad program, and they want you to take care of their houseplants while they're gone.\n
+	if(!testingMode) {
+		alert(`Here's the deal: your friend has taken off on a 40-day study abroad program, and they want you to take care of their houseplants while they're gone.\n
            They're counting on you to keep their plants alive and well! Click on the buttons representing the plants to check each plant's status.\n
            You may only complete a certain number of actions per day. Every action consumes 1 point. Check the "Action Points" section beneath the plants to see how many you have.\n
            Random events that affect your plants' health positively or negatively may occur, so take that into account when caring for the plants!`);
+	}
 }
 
 function showRules(){
@@ -248,7 +278,9 @@ function waterPlant()
     var watered = plantArr[activePlant];
     if(actionPoints == 0)
     {
-        alert("Error: You don't have an action point to spend on this!");
+		if(!testingMode) {
+			alert("Error: You don't have an action point to spend on this!");
+		}
     }
     else if(waterShortage && timesWatered >= 1)
     {
@@ -272,17 +304,23 @@ function setSunPlant()
     var curPlant = plantArr[activePlant];
     if(actionPoints == 0)
     {
-        alert("Error: You don't have an action point to spend on this!");
+		if(!testingMode) {
+			alert("Error: You don't have an action point to spend on this!");
+		}
     }
     else if(curPlant.inSun)
     {
-        alert("Error: That plant is already in the sun!");
+		if(!testingMode) {
+			alert("Error: That plant is already in the sun!");
+		}
     }
     else
     {
         curPlant.inSun = true; // Will cause the plant to gain sun over the days instead of losing it.
         actionPoints--;
-        alert(`You've set plant #${activePlant + 1} in the sun. It will now replenish its sun over the days. Be careful that it does not get too much!`)
+		if(!testingMode) {
+			alert(`You've set plant #${activePlant + 1} in the sun. It will now replenish its sun over the days. Be careful that it does not get too much!`);
+		}
         updateInfo(activeTab);
     }
 }
@@ -292,17 +330,23 @@ function removeSunPlant()
     var curPlant = plantArr[activePlant];
     if(actionPoints == 0)
     {
-        alert("Error: You don't have an action point to spend on this!");
+		if(!testingMode) {
+			alert("Error: You don't have an action point to spend on this!");
+		}
     }
     else if(!curPlant.inSun)
     {
-        alert("Error: That plant is not in the sun!");
+		if(!testingMode) {
+			alert("Error: That plant is not in the sun!");
+		}
     }
     else
     {
         curPlant.inSun = false;
         actionPoints--;
-        alert(`You've removed plant #${activePlant + 1} from the sunlight.`)
+		if(!testingMode) {
+			alert(`You've removed plant #${activePlant + 1} from the sunlight.`);
+		}
         updateInfo(activeTab);
     }
 }
@@ -435,3 +479,191 @@ function asTabs(node) // Appropriated from very old project of Drew's.
     }
     selectTab(0); // to initially show first tab
 }asTabs(document.querySelector("plant-panel"));
+
+function beginTests() {				
+	alert("Testing mode activated, watch the console. The game will need to be restarted after this.");
+	testingMode = true;
+	gameHandler();
+	testPlantCreation();
+	testGameTick(0);
+	testInteraction();
+	soilTest();
+	winLossTest();
+}
+
+function testPlantCreation() {
+	let pass = 1;
+	if(plantArr.length == 4) {
+		for(let i = 0; i < 4; i++) {
+			if(!(plantArr[i].water == 100 && plantArr[i].soil != "Error" && plantArr[i].plantKind != "Error" && plantArr[i].sun == 100 && plantArr[i].living == true && plantArr[i].inSun == false))
+				pass = 0;
+		}
+	}
+	else {
+		pass = 0;
+	}
+	if(pass == 1) {
+		console.log("Plants successfully created");
+	}
+	else {
+		console.log("Plant creation failed");
+	}
+}
+
+function testGameTick(changes) {
+	let pass = 1;
+	let plantSun = [];
+	let plantWater = [];
+	for(let i = 0; i < 4; i++) {
+		plantSun.push(plantArr[i].sun);
+		plantWater.push(plantArr[i].water);
+	}
+	advanceDay();
+	for(let i = 0; i < 4; i++) {
+		if(changes == 0) {
+			if(plantSun[i] <= plantArr[i].sun || plantWater[i] <= plantArr[i].water) {
+					console.log("Unwatered and out-of-sunlight plants' stats do not decrease");
+					pass = 0;
+			}
+		}
+		else if(changes == 1) {
+			if(plantSun[i] >= plantArr[i].sun || plantWater[i] <= plantArr[i].water) {
+					pass = 0;
+					console.log("Plant in sun does not gain sunlight");
+			}
+		}
+		else if(changes == 2) {
+			if(plantSun[i] <= plantArr[i].sun || plantWater[i] >= plantArr[i].water) {
+					pass = 0;
+			}
+		}
+		else {
+			if(plantSun[i] >= plantArr[i].sun || plantWater[i] >= plantArr[i].water) {
+					pass = 0;
+			}	
+		}			
+	}
+	if(pass == 1) {
+		console.log("Day advancement successfully adjusts plant stats");
+	}
+	else {
+		console.log("Day advancement failed updating plant stats");
+	}
+}
+
+function testInteraction() {
+	let pass = 1
+	let testWater = [];
+	actionPoints = 4;
+	for(let i = 0; i < 4; i++) {
+		activePlant = i;
+		setSunPlant();
+	}
+	testGameTick(1);
+	actionPoints = 4;
+	for(let i = 0; i < 4; i++) {
+		activePlant = i;
+		removeSunPlant();
+	}
+	actionPoints = 0;
+	for(let i = 0; i < 4; i++) {
+		activePlant = i;
+		setSunPlant();
+		if(plantArr[i].inSun == true) {
+			pass = 0;
+		}
+	}	
+	if(pass == 0) {
+		console.log("Action points do not disallow changing plant position")
+	}
+	else {
+		console.log("Action points successfully govern plant movement actions");
+	}
+	pass = 1;
+	actionPoints = 0;
+	for(let i = 0; i < 4; i++) {
+		testWater.push(plantArr[i].water);
+		activePlant = i;
+		waterPlant();
+		if(testWater[i] != plantArr[i].water) {
+			pass = 0;
+		}
+	}
+	if(pass == 0) {
+		console.log("Action points do not disallow watering plants");
+	}
+	else {
+		console.log("Action points successfully govern watering actions");
+	}
+}
+
+function soilTest() {
+	waterTest = [];
+	let pass = 0;
+	for(let i = 0; i < 4; i++) {
+		plantArr[i].plantNum = i;
+		plantArr[i].soilNum = i;
+		plantArr[i].water = 100;
+	}
+	advanceDay();
+	for(let i = 0; i < 4; i++) {
+		waterTest.push(100 - plantArr[i].water);
+		plantArr[i].water = 100;
+	}
+	plantArr[0].soilNum = 1;
+	plantArr[1].soilNum = 2;
+	plantArr[2].soilNum = 3;
+	plantArr[3].soilNum = 0;
+	advanceDay();
+	
+	for(let i = 0; i < 4; i++) {
+		if(100 - plantArr[i].water <= waterTest[i]) {
+			pass++;
+		}
+	}
+	
+	if(pass > 2) {
+		console.log("Preferred soil types generally not causing less water loss - due to the rng, this may not be an error - see if this message changes");
+	}
+	else {
+		console.log("Preferred soil types successfully cause generally less water loss");
+	}
+}
+function winLossTest() {
+	curDay = 39;
+	advanceDay();
+	if(winLoss == 1) {
+		console.log("Game is successfully won upon reaching day 40");
+	}
+	else {
+		console.log("Fail, Game is not won upon reaching day 40");
+	}
+	plantArr[0].water = 1;
+	advanceDay();
+	if(winLoss == -1) {
+		console.log("Game is successfully lost upon water reaching 0");
+	}
+	else {
+		console.log("Fail, Game is not lost upon reaching 0 water");
+	}	
+	winLoss = 0;
+	gameHandler();
+	plantArr[0].sun = 1;
+	advanceDay();
+	if(winLoss == -1) {
+		console.log("Game is successfully lost upon sun reaching 0");
+	}
+	else {
+		console.log("Fail, Game is not lost upon reaching 0 sun");
+	}
+	winLoss = 0;
+	gameHandler();
+	plantArr[0].sun = 120;
+	advanceDay();
+	if(winLoss == -1) {
+		console.log("Game is successfully lost upon sun reaching >100");
+	}
+	else {
+		console.log("Fail, Game is not lost upon reaching >100 sun");
+	}
+}
